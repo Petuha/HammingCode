1e3f085995880d3d9aec1d160c88fc2e1aafd814
r3sako <gaikovklim@gmail.com>
2025-01-06 12:00:26 +0300

    Version 6

--- SignalGenerator/SignalGenerator.cpp
+++ SignalGenerator/SignalGenerator.cpp
@@ -1,5 +1,5 @@
 ﻿#include "SignalGenerator.h"
-#include <cmath>     // Для математических операций
+#include <cmath>
 
 std::vector<Dot> generateSignalFromBits(const std::string& bits,
     conversionMethod method, double dt, double A, double bitDuration, bool polarity) 
@@ -9,73 +9,87 @@ std::vector<Dot> generateSignalFromBits(const std::string& bits,
     int stepsPerBit = static_cast<int>(ceil(bitDuration / dt)); // кол-во шагов на один бит
     int globalStep = 0; // счётчик шагов
     double t = 0.0; // тайм текущей точки на X
+    double prevDot = -1.0; // предыдущая точка
 
     for (char bit : bits) {
         switch (method) {
             case conversionMethod::NRZ: {
-                // Non-Return-to-Zero: 0 -> -A, 1 -> +A
                 double amplitude = (bit == '1') ? A : -A;
                 if (polarity) amplitude = -amplitude;
 
-                for (int i = 0; i < stepsPerBit; ++i) {
+                for (int i = 0; i <= stepsPerBit; ++i, ++globalStep) { // <= stepsPerBit для добавления последней точки
                     t = globalStep * dt;
-                    series.emplace_back(t, amplitude);
-                    ++globalStep;
+                    // Проверка на уникальность точки
+                    if (t != prevDot) {
+                        series.emplace_back(t, amplitude);
+                        prevDot = t;
+                    }
                 }
+                --globalStep; // Возвращаемся на 1 шаг назад
                 break;
             }
 
             case conversionMethod::MANCH: {
-                // Manchester: 0 -> [A, -A], 1 -> [-A, A]
                 double firstHalf = (bit == '1') ? -A : A;
                 double secondHalf = -firstHalf;
 
                 int halfSteps = stepsPerBit / 2;
-                for (int i = 0; i < halfSteps; ++i) {
+                for (int i = 0; i < halfSteps; ++i, ++globalStep) {
                     t = globalStep * dt;
-                    series.emplace_back(t, firstHalf);
-                    ++globalStep;
+                    if (t != prevDot) {
+                        series.emplace_back(t, firstHalf);
+                        prevDot = t;
+                    }
                 }
-                for (int i = halfSteps; i < stepsPerBit; ++i) {
+                for (int i = halfSteps; i <= stepsPerBit; ++i, ++globalStep) {
                     t = globalStep * dt;
-                    series.emplace_back(t, secondHalf);
-                    ++globalStep;
+                    if (t != prevDot) {
+                        series.emplace_back(t, secondHalf);
+                        prevDot = t;
+                    }
                 }
+                --globalStep;
                 break;
             }
 
             case conversionMethod::RZ: {
-                // Return-to-Zero: 0 -> [0], 1 -> [A, 0]
                 double amplitude = (bit == '1') ? A : 0;
-                if (polarity && bit == '1') amplitude = -amplitude;
+                if (bit == '1' && polarity) amplitude = -amplitude;
 
                 int halfSteps = stepsPerBit / 2;
-                for (int i = 0; i < halfSteps; ++i) {
+                for (int i = 0; i < halfSteps; ++i, ++globalStep) {
                     t = globalStep * dt;
-                    series.emplace_back(t, amplitude);
-                    ++globalStep;
+                    if (t != prevDot) {
+                        series.emplace_back(t, amplitude);
+                        prevDot = t;
+                    }
                 }
-                for (int i = halfSteps; i < stepsPerBit; ++i) {
+                for (int i = halfSteps; i <= stepsPerBit; ++i, ++globalStep) {
                     t = globalStep * dt;
-                    series.emplace_back(t, 0.0);
-                    ++globalStep;
+                    if (t != prevDot) {
+                        series.emplace_back(t, 0.0);
+                        prevDot = t;
+                    }
                 }
+                --globalStep;
                 break;
             }
 
             case conversionMethod::AMI: {
-                // Alternate Mark Inversion: 0 -> 0, 1 -> alternating +A and -A
                 double amplitude = 0.0;
                 if (bit == '1') {
                     amplitude = lastPolarity ? -A : A;
                     lastPolarity = !lastPolarity;
                 }
 
-                for (int i = 0; i < stepsPerBit; ++i) {
+                for (int i = 0; i <= stepsPerBit; ++i, ++globalStep) {
                     t = globalStep * dt;
-                    series.emplace_back(t, amplitude);
-                    ++globalStep;
+                    if (t != prevDot) {
+                        series.emplace_back(t, amplitude);
+                        prevDot = t;
+                    }
                 }
+                --globalStep;
                 break;
             }
         }
