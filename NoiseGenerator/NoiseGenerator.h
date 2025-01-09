d90eeacc87e8628fa86eed1e65bd31953abc94c2
Pavel Artemov <89926298+Kytaalok@users.noreply.github.com>
2025-01-09 19:19:43 +0500

    Update NoiseGenerator.h

--- NoiseGenerator/NoiseGenerator.h
+++ NoiseGenerator/NoiseGenerator.h
@@ -9,3 +9,5 @@ enum class noiseForm
 };
 std::vector<Dot> generateNoise(int randSeed, const std::vector<Dot>& signal, double t, double dt, int nu, int dnu,
 	noiseForm form, bool polarity, std::vector<double> params);
+double randomDouble(std::mt19937& gen, double minVal, double maxVal);
+double getImpulseValue(ImpulseForm form, double localX, double impulseLength, double aVal, double bVal);
