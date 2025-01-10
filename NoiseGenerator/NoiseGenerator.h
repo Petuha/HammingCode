f80c4364a4badf3b16ab7c592ba2b67ae70f221f
Pavel Artemov <89926298+Kytaalok@users.noreply.github.com>
2025-01-11 00:44:26 +0500

    Update NoiseGenerator.h

--- NoiseGenerator/NoiseGenerator.h
+++ NoiseGenerator/NoiseGenerator.h
@@ -2,39 +2,6 @@
 #include "../SignalGenerator/SignalGenerator.h"
 #include <vector>
 
-
-class RandomParameter {
-private:
-    std::mt19937 gen;
-    std::uniform_real_distribution<double> dist;
-
-public:
-    RandomParameter(unsigned int seed, double minVal, double maxVal)
-        : gen(seed) {
-        dist = std::uniform_real_distribution<double>(minVal, maxVal);
-    }
-
-    double get() {
-        return dist(gen);
-    }
-};
-
-class RandomIntParameter {
-private:
-    std::mt19937 gen;
-    std::uniform_real_distribution<int> dist;
-
-public:
-    RandomIntParameter(unsigned int seed, int minVal, int maxVal)
-        : gen(seed) {
-        dist = std::uniform_real_distribution<int>(minVal, maxVal);
-    }
-
-    double get() {
-        return dist(gen);
-    }
-};
-
 enum class noiseForm
 {
     RECTANGULAR, // прямоугольник
