c9b85fc4f2c97be41abe5afbf30bfe8f1f7a83bb
Pavel Artemov <89926298+Kytaalok@users.noreply.github.com>
2025-01-10 17:53:13 +0500

    Added class RandomGenerator

--- NoiseGenerator/NoiseGenerator.h
+++ NoiseGenerator/NoiseGenerator.h
@@ -1,13 +1,32 @@
 ﻿#pragma once
 #include "../SignalGenerator/SignalGenerator.h"
 #include <vector>
+
+
+class RandomGenerator {
+private:
+    std::mt19937 gen;
+    std::uniform_int_distribution<double> dist;
+public:
+    
+    RandomGenerator(unsigned int seed, double minVal, double maxVal)
+        : gen(seed), dist(minVal, maxVal) {}
+
+    double generate() {
+        return dist(gen);
+    }
+
+    void setRange(double minVal, double maxVal) {
+        dist = std::uniform_int_distribution<double>(minVal, maxVal);
+    }
+};
+
+
 enum class noiseForm
 {
     RECTANGULAR, // прямоугольник
     SINE,        // синусоида
     PARABOLA     // парабола
 };
-std::vector<Dot> generateNoise(int randSeed, const std::vector<Dot>& signal, double t, double dt, int nu, int dnu,
+void generateNoise(int randSeed, std::vector<Dot>& signal, double t, double dt, int nu, int dnu,
 	noiseForm form, bool polarity, std::vector<double> params);
-double randomDouble(std::mt19937& gen, double minVal, double maxVal);
-double getImpulseValue(ImpulseForm form, double localX, double impulseLength, double aVal, double bVal);
