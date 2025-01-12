3758872e1e4efb6b1515e6258129e65ef1bfe277
Mikhail Kotliarov <153776012+karmichael228@users.noreply.github.com>
2025-01-12 18:11:41 +0300

    Update SignalIdentificator.cpp

--- SignalIdentificator/SignalIdentificator.cpp
+++ SignalIdentificator/SignalIdentificator.cpp
@@ -2,36 +2,70 @@
 
 std::string bitsFromSignal(const std::vector<Dot>& signal, conversionMethod method, double dt, double A, double bitDuration, bool polarity) {
     std::string bitSequence;
-    size_t samplesPerBit = static_cast<size_t>(bitDuration / dt);
+    int samplesPerBit = static_cast<int>(ceil(bitDuration / dt));  // Используем ceil для генерации
+    int signalSize = static_cast<int>(signal.size());
+    bitSequence.reserve(signalSize / samplesPerBit);  // Резервируем память
 
-    for (size_t i = 0; i < signal.size(); i += samplesPerBit) {
-        double sumAmplitude = 0.0;
-        for (size_t j = i; j < i + samplesPerBit && j < signal.size(); ++j) {
-            sumAmplitude += signal[j].y;
-        }
-        double averageAmplitude = sumAmplitude / samplesPerBit;
+    for (int i = 0; i < signalSize; i += samplesPerBit) {
+        int countAboveZero = 0, countBelowZero = 0;
+        int halfSamples = samplesPerBit / 2;
 
         switch (method) {
-            case conversionMethod::NRZ:
-                bitSequence += (averageAmplitude > A / 2) ^ polarity ? '1' : '0';
-                break;
-            case conversionMethod::MANCH:
-                bitSequence += (averageAmplitude > A / 2) ? '1' : '0';
-                break;
-            case conversionMethod::RZ:
-                bitSequence += ((i / samplesPerBit) % 2 == 0) ^ polarity ? '1' : '0';
-                break;
-            case conversionMethod::AMI:
-                if (averageAmplitude > A / 2) {
-                    polarity = !polarity;
-                    bitSequence += polarity ? '1' : '1';
-                } else {
-                    bitSequence += '0';
+            case conversionMethod::NRZ: {
+                for (int j = i, cnt = 0; cnt < samplesPerBit; ++j, ++cnt) {
+                    if (signal[j].y > 0) {
+                        ++countAboveZero;
+                    } else {
+                        ++countBelowZero;
+                    }
                 }
+                bitSequence.push_back(((countAboveZero > countBelowZero) ^ polarity) ? '1' : '0');
                 break;
-            default:
-                throw std::invalid_argument("Unknown conversion method");
+            }
+            case conversionMethod::MANCH: {
+                int firstHalfAbove = 0, firstHalfBelow = 0, secondHalfAbove = 0, secondHalfBelow = 0;
+                for (int j = i, cnt = 0; cnt < halfSamples; ++j, ++cnt) {
+                    if (signal[j].y > 0) {
+                        ++firstHalfAbove;
+                    } else {
+                        ++firstHalfBelow;
+                    }
+                }
+                for (int j = i + halfSamples, cnt = 0; cnt < halfSamples; ++j, ++cnt) {
+                    if (signal[j].y > 0) {
+                        ++secondHalfAbove;
+                    } else {
+                        ++secondHalfBelow;
+                    }
+                }
+                bitSequence.push_back((firstHalfBelow > secondHalfBelow) ? '1' : '0');
+                break;
+            }
+            case conversionMethod::RZ: {
+                int firstHalfAbove = 0, firstHalfBelow = 0;
+                for (int j = i, cnt = 0; cnt < halfSamples; ++j, ++cnt) {
+                    if (fabs(signal[j].y) > A / 2) {
+                        ++firstHalfAbove;
+                    } else {
+                        ++firstHalfBelow;
+                    }
+                }
+                bitSequence.push_back((firstHalfAbove > firstHalfBelow) ? '1' : '0');
+                break;
+            }
+            case conversionMethod::AMI: {
+                for (int j = i, cnt = 0; cnt < samplesPerBit; ++j, ++cnt) {
+                    if (fabs(signal[j].y) > A / 2) {
+                        ++countAboveZero;
+                    } else {
+                        ++countBelowZero;
+                    }
+                }
+                bitSequence.push_back((countAboveZero > countBelowZero) ? '1' : '0');
+                break;
+            }
         }
     }
     return bitSequence;
 }
+
