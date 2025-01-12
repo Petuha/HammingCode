cd450a866deee7d42accdd91755ebb2cd4d879e9
Mikhail Kotliarov <153776012+karmichael228@users.noreply.github.com>
2025-01-12 18:12:52 +0300

    Update SignalIdentificator.h

--- SignalIdentificator/SignalIdentificator.h
+++ SignalIdentificator/SignalIdentificator.h
@@ -1,8 +1,6 @@
 ﻿#pragma once
 #include "../SignalGenerator/SignalGenerator.h"
 #include <iostream>
-#include <vector>
-#include <string>
 #include <cmath>
 std::string bitsFromSignal(const std::vector<Dot>& signal,
 	conversionMethod method, double dt, double A, double bitDuration, bool polarity);
